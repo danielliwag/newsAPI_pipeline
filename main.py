154a0a732db42b59574931cc9a5c7bdcc619b311
danielliwag <danielarylliwag@gmail.com>
2025-04-30 18:30:49 +0800

    Added the function to load the data to postgreSQL in the main etl script

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from extract import extract_data
 from transform import transform_data
 from load import load_data_to_postgres
 from log_config import setup_logger
-from config import API_URL, DB_CONFIG
+from config import API_URL
 
 logger = setup_logger(__name__)
 
@@ -11,7 +11,7 @@ def run_etl():
     try:
         raw_data = extract_data(API_URL)
         cleaned_data = transform_data(raw_data)
-        load_data_to_postgres(cleaned_data, 'newsapi', key_column='id', if_exists='append', index=False)
+        load_data_to_postgres(cleaned_data, 'newsapi', key_column='source_id', if_exists='append', index=False)
     except Exception as e:
         logger.exception(f'Problem occured while running pipeline: {e}')
 
